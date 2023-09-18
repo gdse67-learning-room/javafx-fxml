97cf1c9260a39f2d19d5b0833cbc581fde16f76f
danu <danujagreru@gmail.com>
2023-09-18 16:42:41 +0530

    create LauncherWrapper

---
+++ src/main/java/lk/ijse/javafx/LauncherWrapper.java
@@ -0,0 +1,12 @@
+package lk.ijse.javafx;
+
+/*
+    @author DanujaV
+    @created 9/18/23 - 4:40 PM   
+*/
+
+public class LauncherWrapper {
+    public static void main(String[] args) {
+        Launcher.main(args);
+    }
+}
