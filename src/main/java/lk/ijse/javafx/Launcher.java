257cd228e3d31878035715dd4159910746c6dda0
danu <danujagreru@gmail.com>
2023-09-18 16:21:08 +0530

    extends Application to Launcher

---
+++ src/main/java/lk/ijse/javafx/Launcher.java
@@ -0,0 +1,21 @@
+package lk.ijse.javafx;
+
+/*
+    @author DanujaV
+    @created 9/18/23 - 3:56 PM   
+*/
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Launcher extends Application {
+
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
