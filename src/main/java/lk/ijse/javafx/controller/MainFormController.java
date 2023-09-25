bd9d25518815d53c0979b407b1bb61019c9948dc
danu <danujagreru@gmail.com>
2023-09-25 10:32:46 +0530

    onAction

--- src/main/java/lk/ijse/javafx/controller/MainFormController.java
+++ src/main/java/lk/ijse/javafx/controller/MainFormController.java
@@ -5,5 +5,14 @@ package lk.ijse.javafx.controller;
     @created 9/18/23 - 4:03 PM   
 */
 
+import javafx.event.ActionEvent;
+
 public class MainFormController {
+    public void btnCustomerOnAction(ActionEvent actionEvent) {
+        System.out.println("customer button pressed");
+    }
+
+    public void btnItemOnAction(ActionEvent actionEvent) {
+        System.out.println("item button pressed");
+    }
 }
