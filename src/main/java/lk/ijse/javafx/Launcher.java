2494a75e630c8f924ba721e76403a3d54f9cff97
danu <danujagreru@gmail.com>
2023-09-25 09:27:29 +0530

    run a JFX form

--- src/main/java/lk/ijse/javafx/Launcher.java
+++ src/main/java/lk/ijse/javafx/Launcher.java
@@ -9,6 +9,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class Launcher extends Application {
@@ -19,14 +20,20 @@ public class Launcher extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent rootNode = FXMLLoader.load(this.getClass().getResource("/view/main_form.fxml"));
+        //load scene graph to the application
+        AnchorPane rootNode = FXMLLoader.load(this.getClass().getResource("/view/main_form.fxml"));
 
+        //create a new Scene
         Scene scene = new Scene(rootNode);
 
-        stage.setTitle("Main Form");
-        stage.centerOnScreen();
+        //set scene to the primary stage
         stage.setScene(scene);
 
+        //set a title and set center on screen
+        stage.setTitle("Main Form");
+        stage.centerOnScreen();
+
+        //show scene to the crowd
         stage.show();
     }
 }
