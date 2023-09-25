2640e5558b84e2a9d8c3d37c93d802d57d81cf6d
danu <danujagreru@gmail.com>
2023-09-25 11:26:13 +0530

    navigation to customer form

--- src/main/java/lk/ijse/javafx/controller/MainFormController.java
+++ src/main/java/lk/ijse/javafx/controller/MainFormController.java
@@ -6,10 +6,24 @@ package lk.ijse.javafx.controller;
 */
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainFormController {
-    public void btnCustomerOnAction(ActionEvent actionEvent) {
-        System.out.println("customer button pressed");
+    public void btnCustomerOnAction(ActionEvent actionEvent) throws IOException {
+        Parent rootNode = FXMLLoader.load(this.getClass().getResource("/view/customer_form.fxml"));
+
+        Scene scene = new Scene(rootNode);
+
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.setTitle("Customer Form");
+
+        stage.show();
     }
 
     public void btnItemOnAction(ActionEvent actionEvent) {
